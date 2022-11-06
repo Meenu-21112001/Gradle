ec2c96b46c591b5cfc0ac3209c7597a70283275d
Meenu-21112001 <114347759+Meenu-21112001@users.noreply.github.com>
2022-11-06 18:17:32 +0530

    Add files via upload

---
+++ Demo-App/src/main/java/Capg/demo/MavenProject/entity/Student.java
@@ -0,0 +1,22 @@
+package Capg.demo.MavenProject.entity;
+
+
+
+	import lombok.AllArgsConstructor;
+	import lombok.Data;
+	import lombok.NoArgsConstructor;
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public class Student 
+	{
+		
+		private int rollNumber;
+		private String name;
+		private int marks;
+		
+		
+	}
+
+
