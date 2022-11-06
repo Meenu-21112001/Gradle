24d7fd1a0784710b355af86e1435bcf61cd547ff
Meenu-21112001 <114347759+Meenu-21112001@users.noreply.github.com>
2022-11-06 18:32:54 +0530

    Add files via upload

---
+++ SprintBootGradleApp1/src/main/java/com/demo/services/controller/MyWebController.java
@@ -0,0 +1,20 @@
+package com.demo.services.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mybatch")
+public class MyWebController
+{
+
+	@GetMapping("/welcome")
+	public String abc()
+	{
+		return "Welcome Ramesh";
+	}
+	
+}
