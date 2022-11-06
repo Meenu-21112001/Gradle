341689877b3b8ee1fbf38b1ae16498fe643e8095
Meenu-21112001 <114347759+Meenu-21112001@users.noreply.github.com>
2022-11-06 18:28:32 +0530

    Add files via upload

---
+++ ProductGradleAppP/src/main/java/com/mkj/gtest/ProductGradleAppPApplication.java
@@ -0,0 +1,13 @@
+package com.mkj.gtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductGradleAppPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductGradleAppPApplication.class, args);
+	}
+
+}
